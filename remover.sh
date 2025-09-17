e56b08e62e42be6545be66fcf4a622bfb69e443d
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-09-17 10:32:46 -0700

    updated remover.sh so it is suitable for linux machine

--- remover.sh
+++ remover.sh
@@ -1,2 +1,3 @@
-find "/Users/matthijsnuus/Desktop/FS-C/model/injection_model" \
-  -mindepth 1 -maxdepth 1 ! -name 'CO2TAB' -exec rm -rf {} +
+find "/home/manuus/Desktop/FS-C/model/injection_model" -mindepth 1 -maxdepth 1 \
+  \( -name 'INCON' -o -name 'INFILE' -o -name 'MESH' -o -name 'mesh.pickle' -o -name 'CO2TAB' \) -prune -o \
+  -exec rm -rf {} +
