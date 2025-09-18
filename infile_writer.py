9ad4370a2355b46dd8d5ebb1f4fd9853e885a985
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-09-18 14:18:46 -0700

    divided by 100 again

--- infile_writer.py
+++ infile_writer.py
@@ -203,7 +203,7 @@ def generators():
 
     for i in range(len(rel_volumes)): 
         rel_vol = rel_volumes[i]
-        rates = (rates_csv['net flow [kg/s]'] * (rel_vol)).to_list()
+        rates = (rates_csv['net flow [kg/s]'] * 0.001 * (rel_vol)).to_list()
         #rates_co2 = (rates_csv['CO2 rate [kg/s]'] * 1 * (rel_vol)).to_list()
         times = rates_csv['TimeElapsed'].to_list()
 
