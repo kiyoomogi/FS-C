5a6404c56d5edcb6a559fd2f53dd040f9938c4db
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-10-24 12:38:40 -0700

    fault 2m increased volume regular time until end

--- foft_plotter.py
+++ foft_plotter.py
@@ -41,7 +41,7 @@ for f in foft_files:
 
 
 #ax2.plot(date_series, rates_csv.iloc[:, 1], ".-", label="kg/s")
-ax.set_ylim(0,10000)
+ax.set_ylim(0,14000)
 ax.set_xlabel("Date")
 ax.set_ylabel("Pressure [kPa]")
 ax.legend(loc='upper right' , ncol=2, fontsize=8)
