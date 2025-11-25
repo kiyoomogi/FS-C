32a9bea97451dac0a281b875b722beaa9061e0e6
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-11-25 10:07:34 -0800

    stage 0 but simplified injection

--- infile_writer_simpleINJEC.py
+++ infile_writer_simpleINJEC.py
@@ -177,7 +177,7 @@ parameters["rocks"] = {
         "porosity": 0.12,
         #"compressibility": 8e-9,             #Pa^-1
         #"permeability": [2.5e-14, 2.5e-14, 2.5e-14]
-        "permeability": [1e-16,1e-16,1e-16]
+        "permeability": [3e-15,3e-15,3e-15]
     },
     "EDZ  ": {
         "permeability": [1e-13, 1e-13, 1e-13],
