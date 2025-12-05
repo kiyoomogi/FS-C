ae8d95a2761890aeb5c6f00ffa1b8e2896da842c
kiyoomogi <matthijsnuus@gmail.com>
2025-12-05 00:13:41 +0000

    natural state results failed

---
+++ coupled_model/Preprocessing/load_mesh.py
@@ -0,0 +1,23 @@
+import toughio
+import toughflac as tf
+
+
+
+mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_mesh_cyl.msh")
+mesh.cell_data['material'] = mesh.cell_data['material'].ravel()
+
+
+# Import mesh
+tf.model.new()
+tf.zone.import_mesh(mesh)
+
+# Define boundary conditions
+tf.zone.set_dirichlet_bc("BDNTO")
+tf.zone.set_dirichlet_bc("BDNBO")
+
+
+# Export MESH
+tf.zone.export_tough("/home/manuus/Desktop/FS-C/model/coupled_model/MESH")
+
+# Export to f3grid
+tf.zone.export_flac("../mesh.f3grid", binary=True)
