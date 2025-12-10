4f3b4ac8d9f5b888fff163c3a2445e13da659d33
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-12-09 17:30:48 -0800

    fucked

--- coupled_model/3_THM/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./clean.sh
-cp ../MESH .
-#sed "/+++/Q" < ../1_TH_INI/SAVE > INCON
-#echo "" >> INCON
-cp ../flac3d.py .
-cp ../flac3d.sh .
-cp ../tf_in.f3sav tf_in.f3sav
-time mpiexec -n 20 tough3-flac-eco2n INFILE_coupled
