28f531ddb9066bb1ab7701b4347fd028bd04a115
kiyoomogi <matthijsnuus@gmail.com>
2025-12-11 21:51:29 +0000

    result naturatal state no injeciton elements

--- coupled_model/Preprocessing/load_mesh.py
+++ coupled_model/Preprocessing/load_mesh.py
@@ -17,7 +17,7 @@ tf.zone.set_dirichlet_bc("BDNBO")
 
 
 # Export MESH
-tf.zone.export_tough("/home/manuus/Desktop/FS-C/model/coupled_model/MESH")
+#tf.zone.export_tough("/home/manuus/Desktop/FS-C/model/coupled_model/MESH")
 
 # Export to f3grid
 tf.zone.export_flac("../mesh.f3grid", binary=True)
