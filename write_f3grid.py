ce56500d723995d85ccae9eb866e0156ff8f8b59
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-12-16 11:44:15 -0800

    mesh f3grid older version toughio

--- write_f3grid.py
+++ write_f3grid.py
@@ -12,4 +12,4 @@ mesh = toughio.read_mesh("/Users/matthijsnuus/Desktop/FS-C/model/mesh/FSC_mesh_c
 
 materials = mesh.materials 
 
-toughio.write_mesh("/Users/matthijsnuus/Desktop/FS-C/model/coupled_model/mesh.f3grid", mesh, file_format="flac3d", binary=True)
+toughio.write_mesh("/Users/matthijsnuus/Desktop/FS-C/model/coupled_model/mesh.f3grid", mesh, file_format="flac3d")
