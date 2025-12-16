d7aed0099c20c2ef2b5f9ec3bd52b54dac9fd4d7
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2025-12-16 12:39:34 -0800

    natural state older version

--- write_f3grid.py
+++ write_f3grid.py
@@ -13,4 +13,12 @@ mesh = toughio.read_mesh("/Users/matthijsnuus/Desktop/FS-C/model/mesh/FSC_mesh_c
 materials = mesh.materials 
 
 mesh.write_tough("/Users/matthijsnuus/Desktop/FS-C/model/coupled_model/MESH")
+mesh.write("/Users/matthijsnuus/Desktop/FS-C/model/natural_state/mesh.pickle")
+
+
 toughio.write_mesh("/Users/matthijsnuus/Desktop/FS-C/model/coupled_model/mesh.f3grid", mesh, file_format="flac3d")
+
+
+
+mesh = toughio.read_mesh("/Users/matthijsnuus/Desktop/FS-C/model/natural_state/mesh.pickle")
+mesh.read_output("/Users/matthijsnuus/Desktop/FS-C/model/natural_state/OUTPUT_ELEME.csv", time_step=-1)
