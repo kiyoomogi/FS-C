bfe03d48ed2e79f7a90112095a87c35ac4df3ec2
kiyoomogi <matthijsnuus@gmail.com>
2025-12-17 00:12:01 +0000

    results natural state

--- coupled_model/Preprocessing/load_mesh.py
+++ coupled_model/Preprocessing/load_mesh.py
@@ -8,17 +8,18 @@ mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_mesh_cyl.msh"
 #mesh.cell_data['material'] = mesh.cell_data['material'].ravel()
 
 
+
 # Import mesh
 tf.model.new()
-tf.zone.import_mesh(mesh)
+tf.zone.import_mesh(mesh, prune_duplicates=True)
 
 # Define boundary conditions
-tf.zone.set_dirichlet_bc("BDNTO")
-tf.zone.set_dirichlet_bc("BDNBO")
+#tf.zone.set_dirichlet_bc("BDNTO")
+#tf.zone.set_dirichlet_bc("BDNBO")
 
 
 # Export MESH
 #tf.zone.export_tough("/home/manuus/Desktop/FS-C/model/coupled_model/MESH")
 
 # Export to f3grid
-tf.zone.export_flac("../mesh.f3grid")
+#tf.zone.export_flac("../mesh.f3grid")
