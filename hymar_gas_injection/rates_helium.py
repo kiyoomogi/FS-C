d88109d7d5912011fdfb1017511a41977dbf21e4
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2026-01-23 20:56:28 -0800

    INFILE created

--- hymar_gas_injection/rates_helium.py
+++ hymar_gas_injection/rates_helium.py
@@ -59,7 +59,8 @@ df1000["GasRate_kg_s"] = df1000["GasMass_kg"].diff() / df1000["TimeElapsed"].dif
 # Optional: replace NaN in first row with 0
 df1000["GasRate_kg_s"] = df1000["GasRate_kg_s"].fillna(0.0)
 df1000 = df1000[df1000["GasRate_kg_s"] >= 0].reset_index(drop=True)
-
+t0 = df1000["TimeElapsed"].iloc[0]
+df1000["TimeElapsed"] = df1000["TimeElapsed"] - t0
 
 plt.figure(figsize=(8,5))
 plt.plot(df1000["TimeElapsed"], df1000["GasRate_kg_s"], marker="o", linestyle="-")
