406a876ab3673c888bcc0259c6875718d33d390c
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2026-02-02 10:32:58 -0800

    slightly decreased permeability

--- infile_writer_simpleINJEC.py
+++ infile_writer_simpleINJEC.py
@@ -176,7 +176,7 @@ parameters["rocks"] = {
     "FAULT": {
         "porosity": 0.14,
         #"compressibility": 8e-9,             #Pa^-1
-        "permeability": [1e-14, 1e-14, 5e-15],
+        "permeability": [5e-15, 5e-15, 5e-15],
         #"compressibility": 1e-10,             #Pa^-1
     },
 
