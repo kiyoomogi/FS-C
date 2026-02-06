1aa728df72813338777d0f05c94a05e35b01984a
Matthijs Nuus <101344020+kiyoomogi@users.noreply.github.com>
2026-02-06 10:45:39 -0800

    natural state with no BC for BFSB-1

--- natural_state.py
+++ natural_state.py
@@ -131,7 +131,7 @@ parameters["rocks"] = {
     },
     "BFSB1": {
         "porosity": 0.5, #-, (4) 
-        "initial_condition": [BFSB1_value, ini_NACL, ini_gas_content, temperature],
+        #"initial_condition": [BFSB1_value, ini_NACL, ini_gas_content, temperature],
     },
     "FAULT": {
         "porosity": 0.14,
