a83ca499a9323be4b252b0bb054917dddcd2676d
kiyoomogi <matthijsnuus@gmail.com>
2026-02-08 04:23:22 +0000

    8e-14 constnat

--- coupled_model/3_THM/flac3d.py
+++ coupled_model/3_THM/flac3d.py
@@ -199,7 +199,7 @@ python_func_flac = (printer_function,) #(stress_on_plane,)  # After mechanical a
 fish_func_tough = ()  # Before mechanical analysis
 fish_func_flac = ()  # After mechanical analysis
 
-k0_fault = np.array([8.0e-14, 8.0e-14, 1.0e-18], dtype=float)
+k0_fault = np.array([8.0e-15, 8.0e-15, 1.0e-18], dtype=float)
 #k0_fault = np.array([5.0e-18, 5.0e-18, 1.0e-18], dtype=float)
 k0_clay = np.array([5.0e-19, 5.0e-19, 1.0e-19], dtype=float)
 k0_edz = np.array([1.0e-10, 1.0e-10, 1.0e-10], dtype=float)
