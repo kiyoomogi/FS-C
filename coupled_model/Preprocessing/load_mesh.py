4915690eb43c24d543b81ba4343de95d0a3e8ccb
kiyoomogi <matthijsnuus@gmail.com>
2026-03-03 12:47:24 +0000

    now with 2 .f3grid created

--- coupled_model/Preprocessing/load_mesh.py
+++ coupled_model/Preprocessing/load_mesh.py
@@ -2,21 +2,20 @@ import toughio
 import toughflac as tf
 
 
-#mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_mesh_cyl.msh")
-mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_coarse.msh")
+mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_mesh_cyl.msh")
+mesh_coarse = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_coarse.msh")
 
 
 # Import mesh
 tf.model.new()
 tf.zone.import_mesh(mesh , prune_duplicates=True)
+tf.zone.export_flac("../mesh.f3grid")
 
-# Define boundary conditions
-#tf.zone.set_dirichlet_bc("BDNTO")
-#tf.zone.set_dirichlet_bc("BDNBO")
-
+tf.model.new()
+tf.zone.import_mesh(mesh_coarse, prune_duplicates=True)
+tf.zone.export_flac("/home/manuus/Desktop/FS-C/model/coarse_model/coupled_model/mesh.f3grid")
 
 # Export MESH
 #tf.zone.export_tough("/home/manuus/Desktop/FS-C/model/coupled_model/MESH")
 
-# Export to f3grid
-tf.zone.export_flac("../mesh.f3grid")
+
