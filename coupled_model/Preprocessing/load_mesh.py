6acb0e381426df607da83249fc818a411cbdc291
kiyoomogi <matthijsnuus@gmail.com>
2026-03-03 12:27:35 +0000

    coarse mesh

--- coupled_model/Preprocessing/load_mesh.py
+++ coupled_model/Preprocessing/load_mesh.py
@@ -2,15 +2,13 @@ import toughio
 import toughflac as tf
 
 
-mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_mesh_cyl.msh")
-#mesh = toughio.read_mesh("/home/TOUGH3-FLAC3D/toughflac/examples/2DSEP5V6/Preprocessing/gmsh/mesh.msh")
-#mesh.cell_data['material'] = mesh.cell_data['material'].ravel()
-
+#mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_mesh_cyl.msh")
+mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_coarse.msh")
 
 
 # Import mesh
 tf.model.new()
-tf.zone.import_mesh(mesh, prune_duplicates=True)
+tf.zone.import_mesh(mesh , prune_duplicates=True)
 
 # Define boundary conditions
 #tf.zone.set_dirichlet_bc("BDNTO")
