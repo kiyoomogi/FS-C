84e753545c5cec9f73055a3715d092c9e65204fd
kiyoomogi <matthijsnuus@gmail.com>
2026-03-04 18:54:24 +0000

    with different orientation

--- coarse_model/coupled_model/3_THM/flac3d.py
+++ coarse_model/coupled_model/3_THM/flac3d.py
@@ -200,9 +200,9 @@ fish_func_tough = ()  # Before mechanical analysis
 fish_func_flac = ()  # After mechanical analysis
 
 #k0_fault = np.array([8.0e-15, 8.0e-15, 1.0e-18], dtype=float)
-k0_fault = np.array([1.0e-18, 1.0e-18, 1.0e-18], dtype=float)
+k0_fault = np.array([2.0e-18, 2.0e-18, 2.0e-18], dtype=float)
 k0_clay = np.array([5.0e-19, 5.0e-19, 1.0e-19], dtype=float)
-k0_edz = np.array([1.0e-10, 1.0e-10, 1.0e-10], dtype=float)
+k0_edz = np.array([1.0e-13, 1.0e-13, 1.0e-13], dtype=float)
 k0_bnd = np.array([1.0e-18, 1.0e-18, 1.0e-18], dtype=float)
 
 a_fault = 500
@@ -214,9 +214,9 @@ permeability_func = {
         phi0 = 0.14,
         n = 1.0,
         w = 2.0,
-        br = 25e-6,     #was 20e-6
-        bmax = 4100e-6,  #was 500e-6
-        bshear_max = 200e-6,
+        br = 15e-6,     #was 20e-6
+        bmax = 800e-6,  #was 500e-6
+        bshear_max = 300e-6,
         alpha = 0.8,
         n_vector = np.array([0.409576, -0.709406, 0.573576]),
         psi = 10,
