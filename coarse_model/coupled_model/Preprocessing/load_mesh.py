023d34b3510efc52ecc5759cd752defe534aec7e
kiyoomogi <matthijsnuus@gmail.com>
2026-03-12 17:25:20 +0000

    natural state

--- coarse_model/coupled_model/Preprocessing/load_mesh.py
+++ coarse_model/coupled_model/Preprocessing/load_mesh.py
@@ -2,17 +2,10 @@ import toughio
 import toughflac as tf
 
 #mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_coarse.msh")
-mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/simplest.msh")
+mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_coarse.msh")
 
 
 # Import mesh
 tf.model.new()
-tf.zone.import_mesh(mesh) # , prune_duplicates=True)
-tf.zone.export_flac("../mesh.f3grid")
-
-
-# Export MESH and INCON
-tf.zone.export_tough("/home/manuus/Desktop/FS-C/model/coarse_model/coupled_model/MESH")
-
-# Export to f3grid
+tf.zone.import_mesh(mesh , prune_duplicates=True)
 tf.zone.export_flac("../mesh.f3grid")
