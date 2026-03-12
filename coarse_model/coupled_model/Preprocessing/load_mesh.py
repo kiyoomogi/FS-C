2fef7b164f49536a44f15d4b9463ef2be3d69309
kiyoomogi <matthijsnuus@gmail.com>
2026-03-12 12:50:38 +0000

    natural satte

---
+++ coarse_model/coupled_model/Preprocessing/load_mesh.py
@@ -0,0 +1,12 @@
+import toughio
+import toughflac as tf
+
+mesh = toughio.read_mesh("/home/manuus/Desktop/FS-C/model/mesh/FSC_coarse.msh")
+
+
+# Import mesh
+tf.model.new()
+tf.zone.import_mesh(mesh , prune_duplicates=True)
+tf.zone.export_flac("../mesh.f3grid")
+
+
